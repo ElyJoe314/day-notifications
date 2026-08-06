41b1e88c1a6d7c62abb1cbf89281fd638709fec5
ElyJoe314 <ejou2742@gmail.com>
2026-08-06 11:25:51 -0700

    Refactor notification function and use environment variable

--- notify.py
+++ notify.py
@@ -1,8 +1,9 @@
 import requests
 import json
+import os
 from datetime import datetime
 
-TOPIC = "whsCalendarSchedule"
+TOPIC = os.environ["NTFY_TOPIC"]
 
 # Load JSON file
 with open("schooldays.json", "r") as file:
@@ -17,56 +18,7 @@ def get_day_type(date):
     return "Unknown"
 
 
-def send_notification(today, y, m, d):
-    monthDictionary = {
-    "01": "January",
-    "02": "February",
-    "03": "March",
-    "04": "April",
-    "05": "May",
-    "06": "June",
-    "07": "July",
-    "08": "August",
-    "09": "September",
-    "10": "October",
-    "11": "November",
-    "12": "December"
-    }
-    dayDictionary = {
-        "01": "1st",
-        "02": "2nd",
-        "03": "3rd",
-        "04": "4th",
-        "05": "5th",
-        "06": "6th",
-        "07": "7th",
-        "08": "8th",
-        "09": "9th",
-        "10": "10th",
-        "11": "11th",
-        "12": "12th",
-        "13": "13th",
-        "14": "14th",
-        "15": "15th",
-        "16": "16th",
-        "17": "17th",
-        "18": "18th",
-        "19": "19th",
-        "20": "20th",
-        "21": "21st",
-        "22": "22nd",
-        "23": "23rd",
-        "24": "24th",
-        "25": "25th",
-        "26": "26th",
-        "27": "27th",
-        "28": "28th",
-        "29": "29th",
-        "30": "30th",
-        "31": "31st"
-    }
-
-    
+def send_notification(today):
 
     day_type = get_day_type(today)
 
@@ -77,34 +29,31 @@ def send_notification(today, y, m, d):
     else:
         message = "There is no school today"
 
-    requests.post(
+    
+    title = datetime.now().strftime("%B %-d, %Y")
+
+    response = requests.post(
         f"https://ntfy.sh/{TOPIC}",
         data=message,
         headers={
-            "Title": f"{monthDictionary[month]} {dayDictionary[day]}, {year}",
+            "Title": title,
             "Priority": "urgent"
         }
     )
 
+    if response.status_code == 200:
+        print("Notification sent!")
+    else:
+        print("Failed:", response.status_code)
+
     return message
 
 def main():
+    today = datetime.now().strftime("%Y-%m-%d")
+    # today = "2026-09-09"
 
-    year = datetime.now().strftime("%Y")
-    month = datetime.now().strftime("%m")
-    day = datetime.now().strftime("%d")
-    
-    # Testing
-    year = "2026"
-    month = "09"
-    day = "09"
-    
-    # today = datetime.now().strftime("%Y-%m-%d")
-    today = "2026-09-09"
-
-    send_notification(today, year, month, day)
-    print(f"Today's Date: {today}")
-    print(f"Notification Sent: {send_notification(today)}")
+    print(send_notification(today))
+    send_notification(today)
     
 
 if __name__ == "__main__":
